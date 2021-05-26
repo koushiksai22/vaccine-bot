c5d1f88c7b9169e57ced87f26c85cefa5df9d46b
koushiksai22 <39150518+koushiksai22@users.noreply.github.com>
2021-05-26 11:22:51 +0530

    Update entry.py

--- entry.py
+++ entry.py
@@ -7,7 +7,7 @@ pygame.mixer.music.load("sound_dingdong.wav")
 counter = 0
 def get_data():
     url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict"
-    params={"district_id":583,"date":"27-05-2021"}
+    params={"district_id":603,"date":"27-05-2021"}
     headers = {
         'user-agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
     }
